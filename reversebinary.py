c617bd8b7f3d62cbff432befc4fce11bf6a6300f
Andrew Clissold <andrewclissold@gmail.com>
2013-08-18 12:22:40 -0400

    Add to_binary_known_values test case

---
+++ reversebinary.py
@@ -0,0 +1,32 @@
+# reversebinary.py
+
+decimalIn = 0
+decimalOut = 0
+binaryIn = ''
+binaryOut = ''
+
+def main():
+    read_input()
+    to_binary(decimalIn)
+    reverse_binary(binaryIn)
+    to_decimal(binaryOut)
+    display_results(binaryOut)
+
+def read_input():
+    global decimalIn
+    decimalIn = raw_input()
+
+def to_binary(decimal):
+    pass
+
+def reverse_binary(binaryIn):
+    pass
+
+def to_decimal(binary):
+    pass
+
+def display_results(binaryOut):
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ reversebinarytest.py
@@ -0,0 +1,57 @@
+# reversebinarytest.py
+# -*- coding: utf-8 -*-
+
+# Your task will be to write a program for reversing numbers in binary. For
+# instance, the binary representation of 13 is 1101, and reversing it gives
+# 1011, which corresponds to number 11.
+# 
+# Input
+#
+# The input contains a single line with an integer N, 1 ≤ N ≤ 1000000000.
+# 
+# Output
+#
+# Output one line with one integer, the number we get by reversing the binary
+# representation of N.
+
+import unittest
+import reversebinary
+
+class Test(unittest.TestCase):
+
+    known_values = (
+            (1, '1'),
+            (2, '10'),
+            (3, '11'),
+            (4, '100'),
+            (5, '101'),
+            (6, '110'),
+            (7, '111'),
+            (8, '1000'),
+            (16, '10000'),
+            (32, '100000'),
+            (64, '1000000'),
+            (437, '110110101'),
+            (8439, '10000011110111'),
+            (34989, '1000100010101101'),
+            (918574, '11100000010000101110'),
+            (1238974, '100101110011110111110'),
+            (43289709, '10100101001000110001101101'),
+            (900654068, '110101101011101110001111110100'),
+            (1000000000, '111011100110101100101000000000')
+        )
+
+    def test_to_binary_known_values(self):
+        for decimal, binary in self.known_values:
+            found_binary = reversebinary.to_binary(decimal)
+            self.assertEqual(binary, found_binary)
+
+    # def test_reverse_binary(self):
+    #     pass
+    #
+    # def test_to_decimal(self):
+    #     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
