3b85feb2e4aa79f6f3873ea3b1d478423d5f65c8
Andrew Clissold <andrewclissold@gmail.com>
2013-08-19 11:20:53 -0400

    Add more robust comments

--- reversebinary.py
+++ reversebinary.py
@@ -1,11 +1,17 @@
 # reversebinary.py
 
 def main():
+    '''Reverse numbers in binary.
+
+    For instance, the binary representation of 13 is 1101, and reversing it
+    gives 1011, which corresponds to the number 11.
+    '''
+
     decimal_in      = read_input()
     reversed_binary = to_reversed_binary(decimal_in)
     decimal_out     = to_decimal(reversed_binary)
 
-    display_results(decimal_out)
+    print decimal_out
 
 def read_input():
     '''Return a string representation of an integer value'''
@@ -14,6 +20,7 @@ def read_input():
 
 def to_reversed_binary(decimal_in):
     '''Convert a decimal value to its reversed binary (as strings)'''
+    # Input validation
     try:
         value = int(decimal_in)
     except ValueError:
@@ -21,23 +28,30 @@ def to_reversed_binary(decimal_in):
         exit(0)
     if not 1 <= value <= 1000000000:
         raise OutOfRangeError
+
+    # Build a reversed binary representation of a decimal value
     reversed_binary = ''
     while value != 0:
+        # "remainder" will be 0 or 1--the bit to use in the binary string
         remainder = value % 2
-        value /= 2
+        value /= 2 # Integer division
+        # Append the bit on the right of the string, building it in reverse
+        # to avoid unnecessary conversions later
         reversed_binary += str(remainder)
     return reversed_binary
 
 def to_decimal(reversed_binary):
+    '''Convert a binary value to its decimal representation (as strings)'''
     decimal = 0
+    # Scan "reversed_binary" from right to left, building "decimal" using the
+    # binary -> decimal formula (if a given bit == 1, add to a running summation
+    # the base we're converting from raised to that bit's position).
     for i in range(0, len(reversed_binary)):
         if reversed_binary[-i-1] == '1':
             decimal += 2 ** i
     return str(decimal) 
 
-def display_results(decimal_out):
-    print decimal_out
-
+# For input validation
 class OutOfRangeError(ValueError): pass
 
 if __name__ == '__main__':
