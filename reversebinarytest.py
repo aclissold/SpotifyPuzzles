f70efab002d9407dc10b8fe9ffb4afb1c3c2fa54
Andrew Clissold <andrewclissold@gmail.com>
2013-08-19 10:42:03 -0400

    Add to_decimal_known_values test case

--- reversebinarytest.py
+++ reversebinarytest.py
@@ -17,46 +17,50 @@
 import unittest
 import reversebinary
 
+known_values = (
+        ('1', '1'),
+        ('2', '10'),
+        ('3', '11'),
+        ('4', '100'),
+        ('5', '101'),
+        ('6', '110'),
+        ('7', '111'),
+        ('8', '1000'),
+        ('16', '10000'),
+        ('32', '100000'),
+        ('64', '1000000'),
+        ('437', '110110101'),
+        ('8439', '10000011110111'),
+        ('34989', '1000100010101101'),
+        ('918574', '11100000010000101110'),
+        ('1238974', '100101110011110111110'),
+        ('43289709', '10100101001000110001101101'),
+        ('900654068', '110101101011101110001111110100'),
+        ('1000000000', '111011100110101100101000000000')
+    )
+
 class TestToReversedBinary(unittest.TestCase):
 
-    known_values = (
-            ('1', '1'),
-            ('2', '10'),
-            ('3', '11'),
-            ('4', '100'),
-            ('5', '101'),
-            ('6', '110'),
-            ('7', '111'),
-            ('8', '1000'),
-            ('16', '10000'),
-            ('32', '100000'),
-            ('64', '1000000'),
-            ('437', '110110101'),
-            ('8439', '10000011110111'),
-            ('34989', '1000100010101101'),
-            ('918574', '11100000010000101110'),
-            ('1238974', '100101110011110111110'),
-            ('43289709', '10100101001000110001101101'),
-            ('900654068', '110101101011101110001111110100'),
-            ('1000000000', '111011100110101100101000000000')
-        )
 
     def test_to_reversed_binary_known_values(self):
-        for decimal, binary in self.known_values:
+        '''to_reversed_binary should return reversed binary versions of ints'''
+        for decimal, binary in known_values:
             expected = binary[::-1]
             found = reversebinary.to_reversed_binary(decimal)
-            self.assertEqual(found, expected)
+            self.assertEqual(expected, found)
 
     def test_to_reversed_binary_out_of_range(self):
+        '''to_reversed_binary should fail for !(1 <= N <= 1000000000)'''
         for value in ('-1', '0', '1000000001'):
             self.assertRaises(reversebinary.OutOfRangeError,
                     reversebinary.to_reversed_binary, value)
 
-    # def test_reverse_binary(self):
-    #     pass
-    #
-    # def test_to_decimal(self):
-    #     pass
+
+class TestToDecimal(unittest.TestCase):
+    def test_to_decimal_known_values(self):
+        for known_decimal, binary in known_values:
+            found_decimal = reversebinary.to_decimal(binary)
+            self.assertEqual(known_decimal, found_decimal)
 
 
 if __name__ == '__main__':
