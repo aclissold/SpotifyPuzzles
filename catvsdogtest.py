6a60048caa8b7e8a9a1dc7136e628bd82826e31f
Andrew Clissold <andrewclissold@gmail.com>
2013-12-19 17:25:04 -0500

    Update test script

--- catvsdogtest.py
+++ catvsdogtest.py
@@ -1,13 +1,16 @@
-import catvsdogdynamic
+import catvsdog
 import catvsdogbrute
 
 from random import randint
 from time import sleep
 
 def main():
+
+    # XXX RANDOM TESTCASES
+
     total = 0
-    for i in xrange(500):
-        c, d, v = randint(1, 7), randint(1, 7), randint(0, 500)
+    for i in xrange(5000):
+        c, d, v = randint(1, 20), randint(1, 20), randint(0, 500)
         votes = []
         for j in xrange(v):
             cat = randint(1, c)
@@ -17,19 +20,50 @@ def main():
             else:
                 votes.append('D' + str(dog) + ' C' + str(cat))
         brute_answer = catvsdogbrute.run_testcase(c, d, votes)
-        smart_answer = catvsdogdynamic.run_testcase(c, d, votes)
-        print brute_answer, '?=', smart_answer
+        smart_answer = catvsdog.run_testcase(c, d, votes)
         if brute_answer == smart_answer:
             total += 1
-        if i % 5 == 0:
-            print str(int(100*float(i)/500)) + '% complete.'
-    print str(int(100*float(total)/500)) + '% accurate.'
+        else:
+            pass
+        if i % 50 == 0:
+            print str(int(100*float(i)/5000)) + '% complete.'
+    print str(int(100*float(total)/5000)) + '% accurate.'
 
-    #brute_answer = catvsdogbrute.run_testcase(
-    #        1, 2, ['C1 D1', 'C1 D1', 'C1 D2', 'D2 C1'])
-    #smart_answer = catvsdogdynamic.run_testcase(
-    #        1, 2, ['C1 D1', 'C1 D1', 'C1 D2', 'D2 C1'])
-    #print brute_answer, smart_answer
+    # XXX PARTICULAR TESTCASES
+
+#    c, d, v = 1, 2, 3
+#
+    #votes = ['C1 D2',
+    #         'C1 D2',
+    #         'D1 C1']
+    #
+    #votes = [
+    #        'D1 C1',
+    #        'C1 D1',
+    #        'C1 D1',
+    #        'C1 D1'
+    #        ]
+    #votes = [
+    #        'D2 C1',
+    #        'D2 C1',
+    #        'C1 D1',
+    #        'C1 D1',
+    #        'D1 C1',
+    #        ]
+    #votes = [
+    #        'C1 D1',
+    #        'C1 D1',
+    #        'C1 D2',
+    #        'D2 C1'
+    #        ]
+#
+#    print
+#    for vote in votes:
+#        print vote
+#    print
+#    brute_answer = catvsdogbrute.run_testcase(c, d, votes)
+#    smart_answer = catvsdog.run_testcase(c, d, votes)
+#    print 'brute-->', brute_answer, '?=', smart_answer, '<-- real'
 
 if __name__ == '__main__':
     main()
